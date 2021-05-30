5b114fa5bfacd07acd97be9ff8df937752627aee
Exem <chernyakov.ghost@mail.ru>
2021-05-30 22:18:05 +0500

    Добавьте файлы проекта.

---
+++ TRPO1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            funk();
+        }
+        static void funk()
+        {
+            Console.WriteLine("Введите число: "); int a;
+            try
+            {
+                 a = Convert.ToInt32(Console.ReadLine());
+                if (a < 1000000)
+                {
+                    Console.WriteLine(a.ToString("X"));
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Вы ввели не число! / Вы ввели слишком большое число!");
+                funk();
+            }
+
+            
+        }
+    }
+}
